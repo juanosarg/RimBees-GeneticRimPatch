d2369b43e62c36bbd435f9b9c1cdc6627bfd4e85
juanosarg <31105173+juanosarg@users.noreply.github.com>
2019-11-06 14:00:37 +0100

    v0.1

---
+++ Source/RimBeesGeneticPatch/RimBeesGeneticPatch/HediffCompProperties_WhileHavingThoughts.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Text;
+
+namespace RimBeesGeneticPatch
+{
+    class HediffCompProperties_WhileHavingThoughts : HediffCompProperties
+    {
+
+        public List<ThoughtDef> thoughtDefs;
+
+        public HediffCompProperties_WhileHavingThoughts()
+        {
+            this.compClass = typeof(HediffComp_WhileHavingThoughts);
+        }
+    }
+}
