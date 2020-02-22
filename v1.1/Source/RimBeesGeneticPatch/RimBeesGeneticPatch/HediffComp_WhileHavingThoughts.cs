f6d93aabaa8c1725dcfcd9f65e3ff399b5c92c98
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-02-22 16:57:44 +0100

    v1.1

---
+++ v1.1/Source/RimBeesGeneticPatch/RimBeesGeneticPatch/HediffComp_WhileHavingThoughts.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using System.Linq;
+
+namespace RimBeesGeneticPatch
+{
+    class HediffComp_WhileHavingThoughts : HediffComp
+    {
+        public bool flagAmIThinking = false;
+
+        public override void CompExposeData()
+        {
+            Scribe_Values.Look<bool>(ref this.flagAmIThinking, "flagAmIThinking", false, false);
+        }
+
+        public HediffCompProperties_WhileHavingThoughts Props
+        {
+            get
+            {
+                return (HediffCompProperties_WhileHavingThoughts)this.props;
+            }
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            
+            foreach (ThoughtDef thoughtDef in this.Props.thoughtDefs)
+            {
+                flagAmIThinking = false;
+                if (this.Pawn.needs.mood.thoughts.memories.GetFirstMemoryOfDef(thoughtDef) != null)
+                {
+                    flagAmIThinking = true;
+                    break;
+                }
+            }
+
+            if (!flagAmIThinking)
+            {
+                this.Pawn.health.RemoveHediff(this.parent);
+            }
+        }
+
+    }
+}
